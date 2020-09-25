f752cf0ee831889f1fce40ad3eb25c345bb5dc4f
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-09-25 15:22:18 +0900

    :construction: Create necessary configurations

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const outputPath = path.resolve(__dirname, 'build')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: outputPath,
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.md$/,
+                use: 'remark-loader',
+            }
+        ]
+    },
+    devServer: {
+        contentBase: outputPath
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    }
+}
